3cb7cac79b475f21598a11857e08e604b83db69a
blackbox24 <nicardid@gmail.com>
2024-11-27 17:07:00 +0000

    chore: added admin template for add notifications

--- notification/admin.py
+++ notification/admin.py
@@ -9,4 +9,17 @@ class SendNotificationForm(forms.Form):
 
 @admin.register(Notification)
 class NotifiationAdmin(admin.ModelAdmin):
-    pass
+    add_form_template = "admin/custom_add_form.html"
+
+    def add_view(self,request,form_url="",extra_context=None):
+        if request.method == "POST":
+            form = SendNotificationForm(request.POST)
+            if form.is_valid():
+                message = form.cleaned_data["message"]
+                notification = Notification.objects.create(message=message)
+
+        else:
+            form = SendNotificationForm()
+        context = self.get_changeform_initial_data(request)
+        context["form"] = form
+        return super().add_view(request,form_url,extra_context=context)
