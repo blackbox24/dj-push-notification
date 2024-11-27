b9d806880e6e2afe283219d2be18487f552cf89d
blackbox24 <nicardid@gmail.com>
2024-11-27 16:28:40 +0000

    chore: added staticfiles

--- notification/admin.py
+++ notification/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Notification
 
 # Register your models here.
+from django import forms
+
+class SendNotificationForm(forms.Form):
+    message = forms.CharField(label="Notification",max_length=200)
+
+@admin.register(Notification)
+class NotifiationAdmin(admin.ModelAdmin):
+    pass
